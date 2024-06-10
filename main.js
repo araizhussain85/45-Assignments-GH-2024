06c162ae73c01b2dea7aa0534f1565caa6da4490
araizhussain85 <araizhussain85@gmail.com>
2024-06-10 14:22:37 +0500

    Assignment 2
    Assignment 2 is completed which includes Typescript and Javascript files.

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log("Hello World!");
+"use strict";
+let personName = "Araiz,";
+console.log("Ma'am Shumaila: Assalam u alaikum", personName, "would you like to have some lessons on Pharmacognosy today?");
+console.log("Me: Absolutely not.");
--- main.ts
+++ main.ts
@@ -1,1 +1,3 @@
-console.log("Hello World!");
+let personName = "Araiz,";
+console.log("Ma'am Shumaila: Assalam u alaikum", personName,"would you like to have some lessons on Pharmacognosy today?");
+console.log("Me: Absolutely not.");
